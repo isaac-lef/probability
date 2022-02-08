7ff2401a0c1d22fe6d6edb7b38c5867e651be28e
G0atee <isaacbigboss76@gmail.com>
2022-02-08 15:17:23 +0100

    created Chance.java

---
+++ src/main/java/com/g0atee/chance/Chance.java
@@ -0,0 +1,39 @@
+package com.g0atee.chance;
+
+public interface Chance extends Comparable<Chance>{
+	/**
+	 * Access the value stored in this Chance
+	 */
+	public double value();
+
+	/**
+	 * <p>Let's say the current Chance corresponds to an event E. This returns a new Chance corresponding to the complement not(E).</p>
+	 * Example : <code>p</code> is the chance of getting an <i>even</i> number when throwing a dice.<br/>
+	 * then <code>p.complement()</code> is the chance of getting an <i>odd</i> number.
+	 */
+	public Chance complement();
+
+	/**
+	 * <p>This method returns <code>true</code> at random, with a chance <i>matching</i> the value it holds.</p>
+	 * <p>Example:<br/>
+	 * <blockquote>
+	 * <code>Probability p = new Probability(0.5);
+	 * if (p.match()) {
+	 *     // this block has a 50% probability of being executed
+	 * }
+	 * </code>
+	 * <blockquote>
+	 * </p>
+	 */
+	public boolean match();
+
+	/**
+	 * Check if the value is the lowest acceptable one, a.k.a if this chance corresponds to an impossible event.
+	 */
+	public boolean isImpossible();
+
+	/**
+	 * Check if the value is the highest acceptable one, a.k.a if this chance corresponds to an event that happens systematically.
+	 */
+	public boolean isCertain();
+}

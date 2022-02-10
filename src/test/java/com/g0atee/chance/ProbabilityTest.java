cb0d933f1eaccd8fd874fa600d3a5fd9ce493029
G0atee <isaacbigboss76@gmail.com>
2022-02-10 16:47:41 +0100

    more tests for Probability

--- src/test/java/com/g0atee/chance/ProbabilityTest.java
+++ src/test/java/com/g0atee/chance/ProbabilityTest.java
@@ -3,7 +3,11 @@ package com.g0atee.chance;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.stream.IntStream;
 
 public class ProbabilityTest {
 	Probability pImpossible = new Probability(0.0);
@@ -53,4 +57,108 @@ public class ProbabilityTest {
 		Probability p1c = p1.complement();
 		assertEquals(1 - 0.347, p1c.value(), Double.MIN_VALUE);
 	}
+
+	@Test
+	public void matchTest() {
+		final int ITERATIONS = 100_000;
+
+		for (int i=0; i<ITERATIONS; i++) {
+			if (pImpossible.match()) {
+				throw new RuntimeException("an impossible probability should never match.");
+			}
+		}
+
+		for (int i=0; i<ITERATIONS; i++) {
+			if (!pCertain.match()) {
+				throw new RuntimeException("a certain probability should always match.");
+			}
+		}
+
+		int nbMatches = 0;
+		for (int i=0; i<ITERATIONS; i++) {
+			if (pHalf.match()) {
+				nbMatches++;
+			}
+		}
+		double ratio = nbMatches / (double) ITERATIONS;
+		assertEquals(0.5, ratio, 0.01);
+	}
+
+	@Test
+	public void predicateMatchTest() {
+		final int SIZE = 10_000;
+
+		int[] randomInts = IntStream.range(0, SIZE)
+			.filter(pHalf::match)
+			.toArray();
+		double ratio = randomInts.length / (double) SIZE;
+		assertEquals(0.5, ratio, 0.01);
+
+		int[] empty = IntStream.range(0, SIZE)
+			.filter(pImpossible::match)
+			.toArray();
+		assertEquals(0, empty.length);
+
+		int[] full = IntStream.range(0, SIZE)
+		.filter(pCertain::match)
+		.toArray();
+		assertEquals(SIZE, full.length);
+	}
+
+	@Test
+	public void isImpossibleTest() {
+		Probability p1 = new Probability(Double.MIN_VALUE);
+		assertTrue(pImpossible.isImpossible());
+		assertFalse(p1.isImpossible());
+		assertFalse(pHalf.isImpossible());
+		assertFalse(pCertain.isImpossible());
+	}
+
+	@Test
+	public void isCertainTest() {
+		Probability p1 = new Probability(0.999999);
+		assertTrue(pCertain.isCertain());
+		assertFalse(p1.isCertain());
+		assertFalse(pHalf.isCertain());
+		assertFalse(pImpossible.isCertain());
+	}
+
+	@Test
+	public void toProbabilityTest() {
+		assertTrue(pImpossible == pImpossible.toProbability());
+		assertTrue(pHalf == pHalf.toProbability());
+		assertTrue(pCertain == pCertain.toProbability());
+	}
+
+	@Test
+	public void toOddsTest() {
+		Odds oImpossible = pImpossible.toOdds();
+		Odds oHalf       = pHalf.toOdds();
+		Odds oCertain    = pCertain.toOdds();
+		assertTrue(oImpossible instanceof Odds);
+		assertTrue(oHalf       instanceof Odds);
+		assertTrue(oCertain    instanceof Odds);
+		assertEquals(0.0, oImpossible.value(), Double.MIN_VALUE);
+		assertEquals(1.0, oHalf.value(),       Double.MIN_VALUE);
+		assertEquals(Double.POSITIVE_INFINITY, oCertain.value());
+	}
+
+	@Test
+	public void toLogOddsTest() {
+		LogOdds loImpossible = pImpossible.toLogOdds();
+		LogOdds loHalf       = pHalf.toLogOdds();
+		LogOdds loCertain    = pCertain.toLogOdds();
+		assertTrue(loImpossible instanceof LogOdds);
+		assertTrue(loHalf       instanceof LogOdds);
+		assertTrue(loHalf       instanceof LogOdds);
+		assertEquals(Double.NEGATIVE_INFINITY, loImpossible.value());
+		assertEquals(0.0, loHalf.value(), Double.MIN_VALUE);
+		assertEquals(Double.POSITIVE_INFINITY, loCertain.value());
+	}
+
+	// TODO : compareToTest()
+
+	// TODO : equalsTest()
+
+	// TODO : toStringTest()
 }
